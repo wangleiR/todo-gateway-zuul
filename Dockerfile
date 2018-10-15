5e30ef4a0294e177e0cd64518d910919a7f365ed
Lei Wang <lewang@thoughtworks.com>
2018-10-15 16:27:55 +0800

    init zuul

---
+++ Dockerfile
@@ -0,0 +1,7 @@
+FROM anapsix/alpine-java:8
+RUN apk add --no-cache curl
+COPY ./build/libs/*SNAPSHOT.jar app.jar
+ENV JAVA_OPTS=""
+ENV PROFILE="pro"
+EXPOSE 8080
+ENTRYPOINT ["sh", "-c", "java $JAVA_OPTS -Djava.security.egd=file:/dev/./urandom -Dspring.profiles.active=$PROFILE -jar /app.jar"]
---
+++ src/main/java/com/thoughtworks/gateway/GatewayApplication.java
@@ -0,0 +1,23 @@
+package com.thoughtworks.gateway;
+
+import com.thoughtworks.gateway.filter.pre.SimpleFilter;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.cloud.netflix.zuul.EnableZuulServer;
+import org.springframework.context.annotation.Bean;
+
+@EnableZuulProxy
+@EnableZuulServer
+@SpringBootApplication
+public class GatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayApplication.class, args);
+	}
+
+	@Bean
+	public SimpleFilter simpleFilter() {
+		return new SimpleFilter();
+	}
+}
